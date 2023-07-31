e746864d0c8da401df6f9b08a25c9a7e0d2bad26
unknown <chiragjain7052@gmail.com>
2023-07-31 17:11:41 +0530

    name change commit again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CHIGGYY test page
+          CHIGGYY test page testing again
         </p>
         <a
           className="App-link"
